631aed393f90b614a2132b60e8d4fe4e07179b91
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-10-07 09:12:34 -0300

    Validação contador negativo

---
+++ app/src/main/java/com/example/mycontador/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.mycontador
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var contador = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textContador = findViewById<TextView>(R.id.textView)
+        val btnAumentar = findViewById<Button>(R.id.activity_main_btn_aumentar)
+        val btnDiminuir = findViewById<Button>(R.id.activity_main_btn_diminuir)
+
+        btnDiminuir.isEnabled = false
+
+        btnAumentar.setOnClickListener {
+            textContador.text = "${++contador}"
+            btnDiminuir.isEnabled = true
+        }
+
+        btnDiminuir.setOnClickListener {
+            if (contador > 0) {
+                textContador.text = "${--contador}"
+                if (contador == 0) {
+                    btnDiminuir.isEnabled = false
+                }
+            }
+        }
+
+    }
+}
